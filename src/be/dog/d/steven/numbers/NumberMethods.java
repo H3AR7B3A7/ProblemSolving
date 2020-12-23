caa1cf3c0ee475bd9f3a061ad01cc694c2263876
H3AR7B3A7 <steven.d.hondt.sdh@gmail.com>
2020-12-23 23:14:36 +0100

    :sparkles: New problem

--- src/be/dog/d/steven/numbers/NumberMethods.java
+++ src/be/dog/d/steven/numbers/NumberMethods.java
@@ -1,4 +1,46 @@
 package be.dog.d.steven.numbers;
 
+import java.util.HashMap;
+
 public class NumberMethods {
+
+    /**
+     * GIVEN AN ARRAY AND INTEGER K, FIND THE TOTAL NUMBER OF SUB-ARRAYS WITH SUM EQUAL TO K
+     * @param nums The array to check for sub-arrays
+     * @param k The sum the sub-arrays must equal to
+     * @return Number of sub-arrays
+     */
+    public static int numberOfSubArraysEqualToAnIntegerBruteForce(int[] nums, int k) {
+        int res = 0;
+        for (int i = 0; i < nums.length; i++) {
+            int sum = 0;
+            for (int j = i; j < nums.length; j++) {
+                sum += nums[j];
+                if (sum == k)
+                    res++;
+            }
+        }
+        return res;
+    }
+
+    /**
+     * GIVEN AN ARRAY AND INTEGER K, FIND THE TOTAL NUMBER OF SUB-ARRAYS WITH SUM EQUAL TO K
+     * @param nums The array to check for sub-arrays
+     * @param k The sum the sub-arrays must equal to
+     * @return Number of sub-arrays
+     */
+    public static int numberOfSubArraysEqualToAnInteger(int[] nums, int k) {
+        HashMap<Integer, Integer> prevSum = new HashMap<>();
+        int res = 0;
+        int current = 0;
+        for (int num : nums) {
+            current += num;
+            if (current == k)
+                res++;
+            if (prevSum.containsKey(current - k))
+                res += prevSum.get(current - k);
+            prevSum.merge(current, 1, Integer::sum);
+        }
+        return res;
+    }
 }
--- src/be/dog/d/steven/numbers/NumberMethodsTest.java
+++ src/be/dog/d/steven/numbers/NumberMethodsTest.java
@@ -1,7 +1,20 @@
 package be.dog.d.steven.numbers;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class NumberMethodsTest {
 
+    @Test
+    void check_number_of_sub_arrays_equal_to_k(){
+        int[] nums = {1,1,1};
+        assertEquals(2, NumberMethods.numberOfSubArraysEqualToAnIntegerBruteForce(nums,2));
+    }
+
+    @Test
+    void check_number_of_sub_arrays_equal_to_k_optimized(){
+        int[] nums = {1,1,1,5,3,6,2,-1,9,-1,8,1};
+        assertEquals(7, NumberMethods.numberOfSubArraysEqualToAnInteger(nums,8));
+    }
 }
