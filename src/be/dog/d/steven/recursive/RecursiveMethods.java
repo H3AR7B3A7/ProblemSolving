25e344b2810c74a643bac1f9dea46866ffd06c0a
H3AR7B3A7 <steven.d.hondt.sdh@gmail.com>
2020-12-28 22:18:14 +0100

    :recycle: Cleanup

--- src/be/dog/d/steven/recursive/RecursiveMethods.java
+++ src/be/dog/d/steven/recursive/RecursiveMethods.java
@@ -4,6 +4,7 @@ public class RecursiveMethods {
 
     /**
      * REVERSE STRING RECURSIVELY
+     *
      * @param str String to be reversed
      * @return The reverse of str
      */
@@ -16,11 +17,12 @@ public class RecursiveMethods {
 
     /**
      * GET THE SUM OF ALL POSITIVE INTEGERS UP TO N
+     *
      * @param n The target integer
      * @return The sum of all the integers up to n
      */
     public static int getSumOfAllIntsUpToN(int n) {
-        if (n == 0){
+        if (n == 0) {
             return 0;
         } else {
             return (n + getSumOfAllIntsUpToN(n - 1));
@@ -30,49 +32,52 @@ public class RecursiveMethods {
     /**
      * RETURN THE NUMBER OF UNIQUE PATHS IN A WxH ARRAY FROM TOP LEFT TO BOTTOM RIGHT
      * ONLY MOVING DOWN OR RIGHT
-     * @param width Number of columns
+     *
+     * @param width  Number of columns
      * @param height Number of rows
      * @return Number of unique paths
      */
     public static int getNumberOfUniquePaths(int width, int height) {
-        if (width == 1 || height == 1){
+        if (width == 1 || height == 1) {
             return 1;
-        }else{
-            return getNumberOfUniquePaths(width-1, height) + getNumberOfUniquePaths(width, height-1);
+        } else {
+            return getNumberOfUniquePaths(width - 1, height) + getNumberOfUniquePaths(width, height - 1);
         }
     }
 
     /**
      * GET THE VALUE IN THE FIBONACCI SEQUENCE AT A GIVEN POSITION
+     *
      * @param position The position of the value we want to get
      * @return The value at the given position
      */
     public static int getValueAtFibonacciPosition(int position) {
-        if (position == 0){
+        if (position == 0) {
             return 0;
-        } else if (position <= 2){
+        } else if (position <= 2) {
             return 1;
         } else {
-            return getValueAtFibonacciPosition(position-1) + getValueAtFibonacciPosition(position-2);
+            return getValueAtFibonacciPosition(position - 1) + getValueAtFibonacciPosition(position - 2);
         }
     }
 
     /**
      * GET THE NUMBER OF WAYS YOU CAN GROUP AN AMOUNT OF OBJECTS GIVEN A MAX GROUP SIZE
+     *
      * @param numberOfObjects Amount of objects to be grouped
-     * @param maxGroupSize The maximum for individual group sizes
+     * @param maxGroupSize    The maximum for individual group sizes
      * @return Number of possible ways to group up the objects
      */
     public static int getNumberOfWaysToGroupWithMaxGroupSize(int numberOfObjects, int maxGroupSize) {
-        if (numberOfObjects == 0){
+        if (numberOfObjects == 0) {
             return 1;
-        } else if (maxGroupSize == 0 || numberOfObjects < 0){
+        } else if (maxGroupSize == 0 || numberOfObjects < 0) {
             return 0;
         } else {
             return getNumberOfWaysToGroupWithMaxGroupSize(
                     numberOfObjects - maxGroupSize, maxGroupSize) +
                     getNumberOfWaysToGroupWithMaxGroupSize(
-                            numberOfObjects, maxGroupSize -1);
+                            numberOfObjects, maxGroupSize - 1);
         }
     }
 
--- src/be/dog/d/steven/recursive/RecursiveMethodsTest.java
+++ src/be/dog/d/steven/recursive/RecursiveMethodsTest.java
@@ -2,38 +2,40 @@ package be.dog.d.steven.recursive;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class RecursiveMethodsTest {
 
     @Test
-    void reversing_some_string_test(){
-        String reversed = RecursiveMethods.reverseString("test");
-        assertEquals(reversed, "tset");
+    void reversing_some_string_test() {
+        assertEquals("tset", RecursiveMethods.reverseString("test"));
     }
 
     @Test
-    void sum_of_all_non_negative_integers_up_to_n_test(){
-        int actual = RecursiveMethods.getSumOfAllIntsUpToN(5);
-        assertEquals(15,actual);
+    void sum_of_all_non_negative_integers_up_to_n_test() {
+        assertEquals(15, RecursiveMethods.getSumOfAllIntsUpToN(5));
     }
 
     @Test
-    void all_possible_paths_from_top_left_to_bottom_right_test(){
-        int numberOfUniquePaths = RecursiveMethods.getNumberOfUniquePaths(3,4);
-        assertEquals(10,numberOfUniquePaths);
+    void all_possible_paths_from_top_left_to_bottom_right_test() {
+        assertEquals(10, RecursiveMethods.getNumberOfUniquePaths(3, 4));
     }
 
     @Test
-    void value_of_fibonacci_sequence_at_given_position_test(){
-        int actual = RecursiveMethods.getValueAtFibonacciPosition(10);
-        assertEquals(55, actual);
+    void value_of_fibonacci_sequence_at_given_position_test() {
+        assertEquals(55, RecursiveMethods.getValueAtFibonacciPosition(10));
+        assertEquals(0, RecursiveMethods.getValueAtFibonacciPosition(0));
+        assertEquals(1, RecursiveMethods.getValueAtFibonacciPosition(1));
+        assertEquals(1, RecursiveMethods.getValueAtFibonacciPosition(2));
+        assertEquals(2, RecursiveMethods.getValueAtFibonacciPosition(3));
+        assertEquals(3, RecursiveMethods.getValueAtFibonacciPosition(4));
+        assertEquals(5, RecursiveMethods.getValueAtFibonacciPosition(5));
     }
 
     @Test
-    void number_of_ways_to_partition_amount_of_objects_with_given_max_size_of_groups_test(){
-        int numberOfWays = RecursiveMethods.getNumberOfWaysToGroupWithMaxGroupSize(6,4);
-        assertEquals(9, numberOfWays);
+    void number_of_ways_to_partition_amount_of_objects_with_given_max_size_of_groups_test() {
+        assertEquals(9,
+                RecursiveMethods.getNumberOfWaysToGroupWithMaxGroupSize(6, 4));
     }
 
 }
