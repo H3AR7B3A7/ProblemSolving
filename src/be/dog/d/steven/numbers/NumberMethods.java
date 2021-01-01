a2a3dbf20b4b975354644ca082ae2fc346a1e061
H3AR7B3A7 <steven.d.hondt.sdh@gmail.com>
2021-01-01 17:44:45 +0100

    :sparkles: 2 Numbers without double

--- src/be/dog/d/steven/numbers/NumberMethods.java
+++ src/be/dog/d/steven/numbers/NumberMethods.java
@@ -1,13 +1,14 @@
 package be.dog.d.steven.numbers;
 
-import java.util.HashMap;
+import java.util.*;
 
 public class NumberMethods {
 
     /**
      * GIVEN AN ARRAY AND INTEGER K, FIND THE TOTAL NUMBER OF SUB-ARRAYS WITH SUM EQUAL TO K
+     *
      * @param nums The array to check for sub-arrays
-     * @param k The sum the sub-arrays must equal to
+     * @param k    The sum the sub-arrays must equal to
      * @return Number of sub-arrays
      */
     public static int numberOfSubArraysEqualToAnIntegerBruteForce(int[] nums, int k) {
@@ -25,8 +26,9 @@ public class NumberMethods {
 
     /**
      * GIVEN AN ARRAY AND INTEGER K, FIND THE TOTAL NUMBER OF SUB-ARRAYS WITH SUM EQUAL TO K
+     *
      * @param nums The array to check for sub-arrays
-     * @param k The sum the sub-arrays must equal to
+     * @param k    The sum the sub-arrays must equal to
      * @return Number of sub-arrays
      */
     public static int numberOfSubArraysEqualToAnInteger(int[] nums, int k) {
@@ -43,4 +45,49 @@ public class NumberMethods {
         }
         return res;
     }
+
+    /**
+     * IN AN ARRAY OF INTEGERS, FIND THE TWO INTEGERS THAT APPEAR ONLY ONCE
+     *
+     * @param numbers The array containing integers with only 2 appearing once
+     * @return An array with the 2 unique integers
+     */
+    public static int[] findNumbersWithoutDouble(int[] numbers) {
+        int sum = 0;
+        for (Integer number : numbers) {
+            sum = sum ^ number;
+        }
+        sum = sum & -sum;
+        int number1 = 0;
+        int number2 = 0;
+        for (Integer number : numbers) {
+            if ((number & sum) > 0) {
+                number1 = number1 ^ number;
+            } else {
+                number2 = number2 ^ number;
+            }
+        }
+        return new int[]{number1, number2}; // T:O(n), S:O(1)
+    }
+
+    public static int[] findNumbersWithoutDouble2(int[] numbers) {
+        int sum = 0;
+        int memo = 0;
+        for (Integer number : numbers) {
+            memo = sum = sum ^ number;
+        }
+        sum = sum & -sum;
+        int number1 = 0;
+        int number2 = 0;
+        for (Integer number : numbers) {
+            if ((number & sum) > 0) {
+                number1 = number1 ^ number;
+                number2 = memo - number1;
+                break;
+            }
+        }
+        return new int[]{number1, number2}; // T:O(n), S:O(1)
+    }
+
+
 }
--- src/be/dog/d/steven/numbers/NumberMethodsTest.java
+++ src/be/dog/d/steven/numbers/NumberMethodsTest.java
@@ -2,19 +2,39 @@ package be.dog.d.steven.numbers;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class NumberMethodsTest {
 
     @Test
-    void check_number_of_sub_arrays_equal_to_k(){
-        int[] nums = {1,1,1};
-        assertEquals(2, NumberMethods.numberOfSubArraysEqualToAnIntegerBruteForce(nums,2));
+    void check_number_of_sub_arrays_equal_to_k() {
+        int[] nums = {1, 1, 1};
+        assertEquals(2, NumberMethods.numberOfSubArraysEqualToAnIntegerBruteForce(nums, 2));
     }
 
     @Test
-    void check_number_of_sub_arrays_equal_to_k_optimized(){
-        int[] nums = {1,1,1,5,3,6,2,-1,9,-1,8,1};
-        assertEquals(7, NumberMethods.numberOfSubArraysEqualToAnInteger(nums,8));
+    void check_number_of_sub_arrays_equal_to_k_optimized() {
+        int[] nums = {1, 1, 1, 5, 3, 6, 2, -1, 9, -1, 8, 1};
+        assertEquals(7, NumberMethods.numberOfSubArraysEqualToAnInteger(nums, 8));
+    }
+
+    @Test
+    void find_two_numbers_without_double() {
+        int[] nums = {2, 4, 6, 8, 10, 2, 6, 10};
+        assertEquals(Arrays.toString(new int[]{4, 8}), Arrays.toString(NumberMethods.findNumbersWithoutDouble(nums)));
+    }
+
+    @Test
+    void find_two_numbers_without_double2() {
+        int[] nums = {2, 4, 6, 8, 10, 2, 6, 10};
+        assertEquals(Arrays.toString(new int[]{4, 8}), Arrays.toString(NumberMethods.findNumbersWithoutDouble2(nums)));
+    }
+
+    @Test
+    void transitive_closure_test() {
+        int[][] graph = new int[][]{{0, 1, 3}, {1, 2}, {2}, {3}};
+        assertEquals(Arrays.deepToString(new int[][]{}), Arrays.deepToString(graph));
     }
 }
